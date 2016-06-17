fe8b69cc789a31f211d291ebf9c01af866182586
blubs <lgutierrez4435@hotmail.com>
2016-06-16 22:47:36 -0700

    -Added Sound_Sample.cpp / .h for class definition -Implemented initial loading of raw sound file 	(based on example from engine_main.cpp & basic texture loading) 	In the process of deciding upon exact method of usage and how sounds will fit into the engine structure. 	I will be approaching widespread sound integration cautiously until I decide on an approach.

---
+++ app/src/main/jni/Sound_Sample.cpp
@@ -0,0 +1,5 @@
+//
+// Created by F1 on 6/16/2016.
+//
+
+#include "Sound_Sample.h"
---
+++ app/src/main/jni/Sound_Sample.h
@@ -0,0 +1,57 @@
+//
+// Created by F1 on 6/16/2016.
+//
+
+#ifndef ENGINE_SOUND_SAMPLE_H
+#define ENGINE_SOUND_SAMPLE_H
+
+#include "common.h"
+#include "File_Utils.h"
+#include "SL_Utils.h"
+
+class Sound_Sample
+{
+public:
+	const char* raw_data;
+
+	int length = 0;
+
+	int raw_data_length = 0;
+
+	int load(const char* filepath)
+	{
+		//Test audio file loading
+		const Asset_File* sound_file_asset = File_Utils::load_asset("raw_1000_10_5_sec_sweep_stereo.raw");
+		raw_data = (char*) sound_file_asset->data;
+
+		if(!raw_data)
+		{
+			LOGE("Error: failed to load \"%s\"\n",filepath);
+			return 0;
+		}
+		raw_data_length = sound_file_asset->len;
+		length = raw_data_length / sizeof(Stereo_Sample);
+		//We only needed the sound file asset as a container to get the pointers to the actual data
+		free((void*)sound_file_asset);
+
+		//Example loading of test file from engine_main
+		//engine.snd_ch.data = (char*)test_sound_file->data;
+		//We want the length in sound samples
+		//engine.snd_ch.length = (test_sound_file->len)/sizeof(Stereo_Sample);
+		//engine.snd_ch.used = false;
+		//Only needed the Asset_File to get the length
+		//free((void*)test_sound_file);
+
+		return 1;
+	}
+	void unload()
+	{
+		if(raw_data)
+			free((void*)raw_data);
+		raw_data_length = 0;
+	}
+};
+
+
+
+#endif //ENGINE_SOUND_SAMPLE_H
