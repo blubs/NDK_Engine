8f4677c21a5d9c855bd3ed8f221d741bc016878a
blubs <lgutierrez4435@hotmail.com>
2016-07-20 23:06:04 -0700

    -Began configuring application for drawing a test banner advertisement

--- app/src/main/java/engine/games/luisg/engine/Launcher.java
+++ app/src/main/java/engine/games/luisg/engine/Launcher.java
@@ -2,14 +2,11 @@ package engine.games.luisg.engine;
 
 import android.app.NativeActivity;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.res.AssetManager;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
-
+//import com.google.android.gms.ads.*;
+//import com.google.android.gms.ads.adview;
+//import com.google.android.gms.ads.identifier.AdvertisingIdClient;
 
 /**
  * Created by blubs on 7/19/2016.
@@ -17,6 +14,16 @@ import android.util.Log;
 
 public class Launcher extends NativeActivity
 {
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		//Supposedly this should work, however the packages to use these libraries aren't recognized
+//		AdView view = new AdView(this);
+//		view.setAdUnitId("ca-app-pub-3940256099942544~3347511713");
+//		view.setAdSize(AdSize.SMART_BANNER);
+
+	}
 	public boolean test(int a, int b)
 	{
 		return (a==b);
