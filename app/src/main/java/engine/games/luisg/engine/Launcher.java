aea39f8b492926ce5d8bc40c1c57a08256ada211
blubs <lgutierrez4435@hotmail.com>
2016-07-21 23:26:26 -0700

    -Configured project to use Google services -Attempted drawing of test ad, drawing needs to be reconfigured

--- app/src/main/java/engine/games/luisg/engine/Launcher.java
+++ app/src/main/java/engine/games/luisg/engine/Launcher.java
@@ -4,8 +4,12 @@ import android.app.NativeActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
 //import com.google.android.gms.ads.*;
-//import com.google.android.gms.ads.adview;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.AdSize;
 //import com.google.android.gms.ads.identifier.AdvertisingIdClient;
 
 /**
@@ -17,15 +21,40 @@ public class Launcher extends NativeActivity
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
+		loge("JNI: on create started");
 		super.onCreate(savedInstanceState);
-		//Supposedly this should work, however the packages to use these libraries aren't recognized
-//		AdView view = new AdView(this);
-//		view.setAdUnitId("ca-app-pub-3940256099942544~3347511713");
-//		view.setAdSize(AdSize.SMART_BANNER);
 
+		LinearLayout layout = new LinearLayout(this);
+		layout.setOrientation(LinearLayout.VERTICAL);
+
+		//Supposedly this should work, however the packages to use these libraries aren't recognized
+		AdView view = new AdView(this);
+		view.setAdUnitId("ca-app-pub-3940256099942544~3347511713");
+		view.setAdSize(AdSize.SMART_BANNER);
+
+		AdRequest.Builder adRequestBuilder = new AdRequest.Builder();
+		adRequestBuilder.addTestDevice(AdRequest.DEVICE_ID_EMULATOR);
+
+		layout.addView(view);
+		view.loadAd(adRequestBuilder.build());
+		//view.setVisibility(View.VISIBLE);
+
+		setContentView(layout);
+		loge("JNI: on create finished");
 	}
+
+	public void loge(String msg)
+	{
+		Log.println(Log.ERROR,"jni",msg);
+	}
+	public void logi(String msg)
+	{
+		Log.println(Log.INFO,"jni",msg);
+	}
+
 	public boolean test(int a, int b)
 	{
+		loge("test method called with parameters: (" + a + "," + b + ")");
 		return (a==b);
 	}
 }
